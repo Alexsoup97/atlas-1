5088c2588c22131a2517203ffa8085f663ab41ba
Alex Souphanthong <alexsoup8@gmail.com>
2021-04-24 16:03:00 -0400

    final

--- src/index.js
+++ src/index.js
@@ -8,54 +8,71 @@ import {toLonLat} from 'ol/proj';
 import {toStringHDMS} from 'ol/coordinate';
 import {Icon, Style} from 'ol/style';
 import VectorSource from 'ol/source/Vector';
-import Feature, { createStyleFunction } from 'ol/Feature';
+import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import {fromLonLat} from 'ol/proj'
-
-//Shape stuff
 import Circle from 'ol/geom/Circle';
-import {Circle as CircleStyle, Fill, Stroke} from 'ol/style';
+import {Fill, Stroke} from 'ol/style';
+const {derekchauvin, riots, vaccine,genocide} = require('./article.json');
 
 let london = new Feature ({
   geometry: new Point(fromLonLat([-0.12755, 51.507222])),
   name: "london",
   imgsrc: "images/vaccine.png",
-  articleTitle: "uk vaccines",
-  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
+  articleTitle: "Covid vaccine landmark as more than half UK receives initial jab",
+  description: vaccine,
+  avatar:"images/bbc.png",
+  authorName:"John Smith",
+  date:"April 24, 2020"
+  
 });
 
 let minneapolis = new Feature ({
-  geometry: new Point(fromLonLat([-79.3832, 43.6532])),
+  geometry: new Point(fromLonLat([-93.2650, 44.9778])),
   name: "minneapolis",
   imgsrc: "images/derekchauvin.jfif",
-  articleTitle: "Derek Chauvin Guilty LOL",
-  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
+  articleTitle: "Derek Chauvin Verdict Brings a Rare Rebuke of Police Misconduct",
+  description: derekchauvin,
+  avatar:"images/nyt.png",
+  authorName:"John Smith",
+  date:"April 24, 2020"
 });
 
-let vancouver = new Feature ({
-  geometry: new Point(fromLonLat([-123.1207, 49.2827])),
-  name: "vancouver",
+let washington = new Feature ({
+  
+  geometry: new Point(fromLonLat([-77.0369, 38.9072])),
+  name: "washington",
   imgsrc: "images/vancouver.jpg",
-  articleTitle: "something",
-  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
+  articleTitle: "Capitol riot: the terror attack that wasn’t",
+  description: riots,
+  avatar:"images/CNN.jpg",
+  authorName:"John Smith",
+  date:"April 24, 2020"
+
 });
 
 let china = new Feature ({
   geometry: new Point(fromLonLat([90.3413, 35.2827])),
   name: "china",
   imgsrc: "images/uyghurschina.jfif",
-  articleTitle: "uyghurschina",
-  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
+  articleTitle: "China’s Vanishing Muslims: Undercover In The Most Dystopian Place In The World",
+  description: genocide,
+  avatar:"https://yt3.ggpht.com/ytc/AAUvwngVplYvhKizY6Gee3ea3eSaqOMO3xvu3BY37DK4TQ=s48-c-k-c0x00ffffff-no-rj",
+  authorName:"John Smith",
+  date:"April 24, 2020"
 });
 
-const countries = [china, vancouver, london, minneapolis]
+const countries = [china, washington, london, minneapolis]
 
 for (let i = 0; i < countries.length; i++) {
   countries[i].setStyle (
     new Style ({
       image: new Icon ({
         src: 'images/marker.png',
-        scale: 0.08,
+        anchor:[0.5, 640],
+        anchorXUnits:'fraction',
+        anchorYUnits:'pixels',
+        scale: 0.07,
       }),
     })
   );
@@ -70,13 +87,7 @@ let vectorLayer = new VectorLayer ({
 });
 
 
-//SHAPE STUFF
-var image = new CircleStyle({
-  radius: 5,
-  fill: null,
-  stroke: new Stroke({color: 'red', width: 1}),
-});
-
+//RED BUBBLES
 let styles = {
   'Circle': new Style({
     stroke: new Stroke({
@@ -87,15 +98,6 @@ let styles = {
       color: 'rgba(255,0,0,0.2)',
     }),
   }),
-  'yellowCircle': new Style({
-    image: new CircleStyle({
-      radius: 10,
-      fill: null,
-      stroke: new Stroke({
-        color: 'magenta',
-      }),
-    }),
-  })
 };
 
 
@@ -103,14 +105,14 @@ let styleFunction = function (feature) {
   return styles[feature.getGeometry().getType()];
 };
 
-
 let shapeSource = new VectorSource();
 
-shapeSource.addFeature(new Feature(new Circle([4e6, 1e6], 1000000)));
-shapeSource.addFeature(new Feature(new Circle([1e6, 3e6], 1000000)));
-shapeSource.addFeature(new Feature(new Circle([2e6, 7e6], 1000000)));
-shapeSource.addFeature(new Feature(new Circle([7e6, 8e6], 1000000)));
-shapeSource.addFeature(new Feature(new Circle([-10e6, 5e6], 1000000)));
+shapeSource.addFeature(new Feature(new Circle(fromLonLat([30.8025, 26.8206]), 1000000)));
+shapeSource.addFeature(new Feature(new Circle(fromLonLat([-60.0217, -3.1190]), 1000000)));
+shapeSource.addFeature(new Feature(new Circle(fromLonLat([116.4074,39.9042]), 1000000)));
+
+
+
 
 let shapeLayer = new VectorLayer ({
   source: shapeSource,
@@ -144,7 +146,6 @@ let map = new Map({
   }),
 });
 
-
 closer.onclick = () => {
   overlay.setPosition(undefined);
   closer.blur();
@@ -159,8 +160,9 @@ map.on('click', (event) => {
   });
 
   if (feature) {
-    content.innerHTML = ``
-    content.innerHTML = `<h6>${hdms}</h6><h3>${feature.get('articleTitle')}</h3><img src="${feature.get('imgsrc')}"><h6>${feature.get('description')}</h6>`;
+    content.innerHTML = `<div class=left-preview id =${feature.get('name')}></div><div class = right-preview><h4>${hdms}</h4><h2>${feature.get('articleTitle')}</h2>
+    <h6>${feature.get('description')}</h6><div class=author><img src= ${feature.get('avatar')} width =40 height = 40><div class = name><h4>  ${feature.get('authorName')}</h4><p>  ${feature.get('date')}</div></div></div>`;
+
     overlay.setPosition(coordinate);
   } else {
     overlay.setPosition(undefined);
