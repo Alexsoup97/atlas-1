9e4bd0c6201f221cb896b5a8c80d117d0672642f
nickyc11 <chew.nicky@gmail.com>
2021-04-24 04:17:10 -0400

    temp

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Atlas is a platform that gathers relevant worldwide news from various types
+# ATLAS
+### A web platform that gathers relevant worldwide news across various genres and precisely maps articles to their respective locations. Through the interactive layout, users gain further insight and visual awareness into pertinent current events. 
+
+### Currently ATLAS is a front-end concept, however further development will likely involve more web frameworks and API integration to retrieve the news articles, and a database to store user preferences.
--- src/index.js
+++ src/index.js
@@ -8,68 +8,121 @@ import {toLonLat} from 'ol/proj';
 import {toStringHDMS} from 'ol/coordinate';
 import {Icon, Style} from 'ol/style';
 import VectorSource from 'ol/source/Vector';
-import Feature from 'ol/Feature';
+import Feature, { createStyleFunction } from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import {fromLonLat} from 'ol/proj'
 
-var london = new Feature({
+//Shape stuff
+import Circle from 'ol/geom/Circle';
+import {Circle as CircleStyle, Fill, Stroke} from 'ol/style';
+
+let london = new Feature ({
   geometry: new Point(fromLonLat([-0.12755, 51.507222])),
+  name: "london",
+  imgsrc: "images/vaccine.png",
+  articleTitle: "uk vaccines",
+  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
 });
 
-london.setStyle(
-  new Style({
-    image: new Icon({
-      crossOrigin: 'anonymous',
-      src: 'marker.png',
-      scale: 0.08,
-    }),
-  })
-);
-
-var toronto = new Feature({
+let minneapolis = new Feature ({
   geometry: new Point(fromLonLat([-79.3832, 43.6532])),
-  name: "toronto"
+  name: "minneapolis",
+  imgsrc: "images/derekchauvin.jfif",
+  articleTitle: "Derek Chauvin Guilty LOL",
+  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
 });
 
-toronto.setStyle(
-  new Style({
-    image: new Icon({
-      crossOrigin: 'anonymous',
-      src: 'marker.png',
-      scale: 0.08,
-    }),
-  })
-);
-
-var iconFeature = new Feature({
+let vancouver = new Feature ({
   geometry: new Point(fromLonLat([-123.1207, 49.2827])),
-
+  name: "vancouver",
+  imgsrc: "images/vancouver.jpg",
+  articleTitle: "something",
+  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
 });
 
-var iconStyle = new Style({
-  image: new Icon({
-    
-    scale: 0.08,
-    src: 'marker.png',
-  }),
+let china = new Feature ({
+  geometry: new Point(fromLonLat([90.3413, 35.2827])),
+  name: "china",
+  imgsrc: "images/uyghurschina.jfif",
+  articleTitle: "uyghurschina",
+  description: "aasdlkjfhasdklfjhasldkfjhasldkfj\nas;dklfjas;dlkfja;\n"
 });
 
-iconFeature.setStyle(iconStyle);
+const countries = [china, vancouver, london, minneapolis]
 
+for (let i = 0; i < countries.length; i++) {
+  countries[i].setStyle (
+    new Style ({
+      image: new Icon ({
+        src: 'images/marker.png',
+        scale: 0.08,
+      }),
+    })
+  );
+}
 
-var vectorSource = new VectorSource({
-  features: [iconFeature, london, toronto]
+let vectorSource = new VectorSource ({
+  features: countries
 });
 
-var vectorLayer = new VectorLayer({
+let vectorLayer = new VectorLayer ({
   source: vectorSource,
 });
 
-var container = document.getElementById('popup');
-var content = document.getElementById('popup-content');
-var closer = document.getElementById('popup-closer');
 
-var overlay = new Overlay({
+//SHAPE STUFF
+var image = new CircleStyle({
+  radius: 5,
+  fill: null,
+  stroke: new Stroke({color: 'red', width: 1}),
+});
+
+let styles = {
+  'Circle': new Style({
+    stroke: new Stroke({
+      color: 'red',
+      width: 2,
+    }),
+    fill: new Fill({
+      color: 'rgba(255,0,0,0.2)',
+    }),
+  }),
+  'yellowCircle': new Style({
+    image: new CircleStyle({
+      radius: 10,
+      fill: null,
+      stroke: new Stroke({
+        color: 'magenta',
+      }),
+    }),
+  })
+};
+
+
+let styleFunction = function (feature) {
+  return styles[feature.getGeometry().getType()];
+};
+
+
+let shapeSource = new VectorSource();
+
+shapeSource.addFeature(new Feature(new Circle([4e6, 1e6], 1000000)));
+shapeSource.addFeature(new Feature(new Circle([1e6, 3e6], 1000000)));
+shapeSource.addFeature(new Feature(new Circle([2e6, 7e6], 1000000)));
+shapeSource.addFeature(new Feature(new Circle([7e6, 8e6], 1000000)));
+shapeSource.addFeature(new Feature(new Circle([-10e6, 5e6], 1000000)));
+
+let shapeLayer = new VectorLayer ({
+  source: shapeSource,
+  style: styleFunction
+})
+
+//POPUP
+let container = document.getElementById('popup');
+let content = document.getElementById('popup-content');
+let closer = document.getElementById('popup-closer');
+
+let overlay = new Overlay({
   element: container,
   autoPan: true,
   autoPanAnimation: {
@@ -77,41 +130,39 @@ var overlay = new Overlay({
   },
 });
 
-var map = new Map({
+let map = new Map({
   layers: [new TileLayer({
     source: new OSM()
-  }),vectorLayer], 
+  }),
+  vectorLayer,
+  shapeLayer], 
   overlays: [overlay],
   target: document.getElementById('map'),
   view: new View({
     center: [0, 0],
     zoom: 2,
-    controls: []
   }),
 });
 
-//map.addLayer(EUCountriesGeoJSON)
 
-closer.onclick = function () {
+closer.onclick = () => {
   overlay.setPosition(undefined);
   closer.blur();
   return false;
 };
 
-map.on('click', function (evt) {
-  var coordinate = evt.coordinate;
-  var hdms = toStringHDMS(toLonLat(coordinate));
-  var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+map.on('click', (event) => {
+  let coordinate = event.coordinate;
+  let hdms = toStringHDMS(toLonLat(coordinate));
+  let feature = map.forEachFeatureAtPixel(event.pixel, (feature) => {
     return feature;
   });
 
-  if(feature){
-
-    content.innerHTML = '<p>ARTICLE</p><img src="download.jfif">';
+  if (feature) {
+    content.innerHTML = ``
+    content.innerHTML = `<h6>${hdms}</h6><h3>${feature.get('articleTitle')}</h3><img src="${feature.get('imgsrc')}"><h6>${feature.get('description')}</h6>`;
     overlay.setPosition(coordinate);
-    console.log(feature.get('name'));
-  }else{
-
+  } else {
     overlay.setPosition(undefined);
     closer.blur(); 
   }
